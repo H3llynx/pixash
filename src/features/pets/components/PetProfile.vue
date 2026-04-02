f3af9429db7ebf769ec3f820e1de6b828a27f6c6
Helene T <helene.teychene@gmail.com>
2026-04-02 21:05:32 +0200

    feature / started pet profile content

--- src/features/pets/components/PetProfile.vue
+++ src/features/pets/components/PetProfile.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { usePets } from '../composable/usePet';
+import { getAge, getIcon } from '../utils';
 
 const { selectedPet } = usePets();
 </script>
@@ -7,7 +8,45 @@ const { selectedPet } = usePets();
 <template>
     <section v-if="selectedPet" class="pet-section">
         <h2>{{ selectedPet.name }}'s profile</h2>
-        <div class="flex flex-col gap-0.5 p-1 rounded-xl border border-border bg-bg-2">
+        <div class="flex flex-col gap-0.5 p-1 rounded-xl border border-border bg-bg-2 capitalize">
+            <div class="flex justify-between items-center">
+                <div class="rounded-full w-4 h-4 bg-brand-rgba text-4xl flex shrink-0 justify-center items-center">
+                    {{ getIcon(selectedPet) }}
+                </div>
+                <div class="mr-auto p-1 text-text-secondary text-sm">
+                    <h1>{{ selectedPet.name }}</h1>
+                    <span v-if="selectedPet.breed">{{ selectedPet.breed }} · </span>
+                    <span>{{ getAge(selectedPet) }} years old · </span>
+                    <span>{{ selectedPet.sex }}</span>
+                </div>
+            </div>
+            <div class="text-sm">
+                <div class="row">
+                    <span>Weight</span>
+                </div>
+                <div class="row">
+                    <span>Next vaccine</span>
+                </div>
+                <div class="row">
+                    <span>Microchip</span>
+                </div>
+            </div>
         </div>
     </section>
 </template>
+
+<style scoped>
+.row {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 0;
+
+    span:first-child {
+        color: var(--color-text-secondary);
+    }
+
+    &:not(:last-child) {
+        border-bottom: 1px solid var(--color-separator)
+    }
+}
+</style>
--- src/features/user/components/UserPicture.vue
+++ src/features/user/components/UserPicture.vue
@@ -14,7 +14,7 @@ const menuBtnRef = ref<HTMLButtonElement | null>(null);
         <button tabindex="0" @click="visible = !visible" v-if="user" ref="menuBtnRef"
             class="rounded-full w-3.5 h-3.5 shrink-0 overflow-hidden bg-brand flex items-center justify-center">
             <img v-if="user.photo" :src="user.photo" :alt="user.firstName ?? 'user avatar'" />
-            <p v-else class="text-gold capitalize text-3xl">{{ user.firstName.slice(0, 1) }}
+            <p v-else class="text-text-chip capitalize text-3xl">{{ user.firstName.slice(0, 1) }}
             </p>
         </button>
         <UserMenu v-model:visible="visible" :toggleRef="menuBtnRef" />
