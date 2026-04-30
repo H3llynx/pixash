7ddd7552b83c0439e598aa60c3a509ee40fc7b9d
Helene T <helene.teychene@gmail.com>
2026-04-30 14:35:53 +0200

    style: dropped semi transparent background for the edition mode

--- src/components/FormWrapper.vue
+++ src/components/FormWrapper.vue
@@ -14,12 +14,9 @@ onMounted(() => {
 })
 </script>
 
-
 <template>
-    <section :class="[
-        readonly ? 'bg-bg-rgba filter-blur' : 'bg-bg-3',
-        'sticky bottom-0 right-0 z-2 border-t md:border border-border rounded-t-3xl pt-1 pb-2 px-0 md:max-w-2xl md:ml-auto mt-3'
-    ]">
+    <section
+        class="bg-bg-3 sticky bottom-0 right-0 z-2 border-t md:border border-border rounded-t-3xl pt-1 pb-2 px-0 md:max-w-2xl md:ml-auto mt-3">
         <Button v-if="canClose" action="hide" @click="onClose" />
         <slot />
     </section>
