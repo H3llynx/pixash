2447c57469058c5081d42dc5c28c326c2d7bb8d6
Helene <helene.teychene@gmail.com>
2026-06-25 14:52:31 +0200

    replaced disabled by v-show

--- src/components/AddPictures.vue
+++ src/components/AddPictures.vue
@@ -22,11 +22,11 @@ const { pictures, onFileChange, deletePicture } = useAddPictures();
             <X :size="20" />
         </Button>
     </div>
-    <label for="pictures" :aria-label="t('common.fileInputLabel')"
-        class="w-[160px] h-[120px] mb-0.25 border border-dashed border-text-secondary text-text-secondary rounded-xl flex items-center gap-[5px] justify-center p-0.5 hover:text-brand-light hover:border-brand-light has-disabled:cursor-not-allowed has-disabled:opacity-40">
+    <label for="pictures" :aria-label="t('common.fileInputLabel')" v-show="pictures.length + initialImages < max"
+        class="w-[160px] h-[120px] mb-0.25 border border-dashed border-text-secondary text-text-secondary rounded-xl flex items-center gap-[5px] justify-center p-0.5 hover:text-brand-light hover:border-brand-light">
         <input id="pictures" type="file" accept="image/*" class="sr-only" tabindex="0" multiple
-            @change="onFileChange($event, max, initialImages)" @click="(e) => (e.target as HTMLInputElement).value = ''"
-            :disabled="pictures.length + initialImages >= max" />
+            @change="onFileChange($event, max, initialImages)"
+            @click="(e) => (e.target as HTMLInputElement).value = ''" />
         <Camera class="shrink-0" />
     </label>
 </template>
